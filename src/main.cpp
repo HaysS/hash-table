3f0ce96ff7e20b40ea178b2bd82cf6afdb1bf64a
Hays Stanford <stanfordhays@gmail.com>
2020-01-04 16:03:01 -0600

    renamed Hash class to Hashtable

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include "Hash.h"
+#include "HashTable.h"
 
 const int TABLE_SIZE = 10;
 
 int main() {
 	char str[] = "Hello World!";
 	
-	Hash hash;
-	int index = hash.index(TABLE_SIZE, str, sizeof(str));
+	HashTable hashTable;
+	int index = hashTable.index(TABLE_SIZE, str, sizeof(str));
 
 	std::cout << index;
 
